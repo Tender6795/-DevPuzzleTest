d062ae3215107fad002697542bc1e9d63022c7c5
Muchnik Ilya <tender6795@gmail.com>
2019-08-21 13:18:43 +0300

    start project

---
+++ server/config/index.js
@@ -0,0 +1,7 @@
+const config={
+  database:'mongodb://localhost:27017/vacationCalendar',
+  port:4000,
+  secret:"verysecretkey",
+};
+
+export default config;
---
+++ server/server.js
@@ -0,0 +1,45 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import session from 'express-session';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import bluebird from 'bluebird';
+
+import config from "./config";
+import errorHandler from './middlewares/errorHandler';
+
+
+
+
+
+
+const app = express();
+mongoose.Promise = bluebird;
+
+mongoose.connect(config.database, {useNewUrlParser: true}, err => {
+  if (err) throw err;
+  console.log('Mongo connected');
+});
+
+const server = app.listen(config.port, err => {
+  if (err) throw err;
+
+  console.log(`Server listening on port ${config.port}`);
+});
+
+app.use(morgan('tiny'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(session({
+  resave: true,
+  saveUninitialized: true,
+  secret: config.secret
+}));
+
+
+
+app.use(errorHandler);
+
+
+
+export default server;
